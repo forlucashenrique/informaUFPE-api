8849ac5425b9c7707d283e33aaa6fb73f4c4b61b
Lucas Henrique <forlucashenrique@gmail.com>
2022-11-10 17:45:09 -0300

    removendo os espaços dos items caso o resultado seja um string vazia

--- pages/api/cardapio/index.js
+++ pages/api/cardapio/index.js
@@ -2,17 +2,15 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 
-const pegarCardapio = async (res) => {
+const getMenu = async (res) => {
   const cardapio = {};
   const result = {}
   const url = 'https://www.ufpe.br/rucaa';
   const response = await axios.get(url);
   const html = response.data;
   const $ = cheerio.load(html);
-  const navCabecalho = $('.tabs nav');
-  const spansDias = navCabecalho.children();
-
-  let id = 0;
+  const spansDias = $('.tabs nav span');
+  // const spansDias = navCabecalho.children();
 
   spansDias.each(function(){
     const nomeDia = $(this).text().trim().toLowerCase();
@@ -27,7 +25,7 @@ const pegarCardapio = async (res) => {
     if(nomeDia === 'Sexta'){
       tbody = $('tbody', tabelaTres);
     } 
-    else if(nomeDia === 'Quarta' || nomeDia === 'Quinta') {
+    else if (nomeDia === 'Quarta' || nomeDia === 'Quinta') {
       tbody = $('tbody', tabelaSecundaria);
     }
     else {
@@ -43,16 +41,15 @@ const pegarCardapio = async (res) => {
     tr.each(function(){
       const almocoColuna = $('td:first-child', this);
       const jantarColuna = $('td:last-child', this);
-      const ingredienteAlmoco = $(almocoColuna).text();
-      const ingredienteJantar = $(jantarColuna).text();
+      const ingredienteAlmoco = $(almocoColuna).text().trim();
+      const ingredienteJantar = $(jantarColuna).text().trim();
 
       ingredientesAlmoco.push(ingredienteAlmoco);
       ingredientesJantar.push(ingredienteJantar);
       
     })
-  
-
-    result[nomeDia] =  {'almoco': ingredientesAlmoco, 'janta': ingredientesJantar}
+    
+    result[nomeDia] =  {'almoco': ingredientesAlmoco, 'janta': ingredientesJantar};
 
   })
 
@@ -64,7 +61,7 @@ const pegarCardapio = async (res) => {
 export default function Cardapio(req, res){
   try {
     
-    pegarCardapio(res)
+    getMenu(res)
   
   } catch(err) {
     res.status(400).json({'Error': err})
