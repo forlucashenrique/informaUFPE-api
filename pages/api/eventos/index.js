690852b64fe7d561c566011458c3747d9f87b728
Lucas Henrique <forlucashenrique@gmail.com>
2024-09-16 19:36:39 -0300

    adjust events endpoint

--- pages/api/eventos/index.js
+++ pages/api/eventos/index.js
@@ -1,12 +1,45 @@
 import api from '../../../service/api';
 import cheerio from 'cheerio';
+import https from 'https';
 
+
+
+
+function formattedMarkedDates(event) {
+  const date = event.createdAt.split(' ')[0]
+
+  const markedDate = {
+      [date]: {
+          marked: true,
+          dotColor: '#262D33',
+          dots: [{
+              key: 'meeting',
+              color: '#262D33',
+          }]
+      }
+  }
+
+  return markedDate
+}
 
 export default async function ListEvents(req, res) {
+
+  function formatDate(dateTime) {
+    const dateTimeSplited = dateTime.split(' ')
+    const dateSplited = dateTimeSplited[0].split('/')
+    const markedDates = {}
+
+
+    return `${dateSplited[2]}-${dateSplited[1]}-${dateSplited[0]} ${dateTimeSplited[1]}`
+
+  }
+
   try {
     const events = {};
     const result = [];
-    const response = await api.get('/caa/eventoscaa');
+    const response = await api.get('/caa/eventoscaa', {
+      httpsAgent: new https.Agent({ rejectUnauthorized: false })
+    });
     const html = response.data;
     const $ = cheerio.load(html);
     const divListEvents = $('.list-events');
@@ -17,9 +50,10 @@ export default async function ListEvents(req, res) {
 
     itensEvents.each(function(){
       const createdEventAt = $('time', this).attr('datetime'); 
+
       const linkEvent = $('a', this).attr('href');
-      const titleEvent = $('h4', this).text();
-      const localeEvent = $('p', this).text();
+      const titleEvent = $('h4', this).text().trim();
+      const localeEvent = $('p', this).text().trim();
 
       if(titleEvent){
         const linkSplitedOne = linkEvent.split('?')
@@ -30,7 +64,7 @@ export default async function ListEvents(req, res) {
           'title': titleEvent,
           'idEvent': linkSplitedTwo[linkSplitedTwo.length - 1],
           'locale': localeEvent,
-          'createdAt': createdEventAt,
+          'createdAt': formatDate(createdEventAt),
         })
         
         id += 1
@@ -38,6 +72,24 @@ export default async function ListEvents(req, res) {
     })
 
     events['result'] = result
+    events['markedDates'] = {}
+
+    events.result.forEach(event => {
+      const date = event.createdAt.split(' ')[0]
+      const markedDate = {
+          [date]: {
+              marked: true,
+              dotColor: '#262D33',
+              dots: [{
+                  key: 'meeting',
+                  color: '#262D33',
+              }]
+          }
+      }
+
+      events.markedDates = {...events.markedDates, ...markedDate}
+    })
+
     res.setHeader(
       'Cache-Control',
       's-maxage=86400',
